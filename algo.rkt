f9b4d829bbb2ec41c274291c4b5abe3c530898bb
Rhushabh Gedam <rhushabhgedam3@gmail.com>
2019-04-04 01:14:24 +0530

    ashish ki vines

--- algo.rkt
+++ algo.rkt
@@ -3,8 +3,8 @@
 
 (provide all-defined-out)
 
-;(require examples.rkt)
-;(require functions.rkt)
+(require "examples.rkt")
+(require "trivial func.rkt")
 (require racket/mpair)
 
 (define INF 1000000)
@@ -18,7 +18,7 @@
 (define ROW 10)
 (define COL 10)
 
-;openlist having each element as '(f (cons i j))
+;openlist having each element as '(f i j)
 (define openList '())
 
 ;2-d grid of boolean values (#t if cell is processed)
@@ -34,6 +34,10 @@
 ;goto (i j) coordinates of vec
 (define (vec-ref vec i j)
   (vector-ref (vector-ref vec i) j))
+
+;set vec
+(define (vec-set! vec r c val)
+  (vector-set! (vector-ref vec r) c val))
 
 ;traces path from dest to source
 (define (tracePath cellDetails dest)
@@ -54,6 +58,64 @@
   (begin
     (displayln "The path is :")
     (printPath (parent-follower (car dest) (cdr dest) '()))))
+
+;main A* algo
+(define foundDest #f)
+
+(define (aStarSearch grid src dest)
+
+  (define (looper)
+    (cond [(null? openList) (if foundDest (void 1) "Failed to find the Destination Cell")]
+          [else (let* ([top (car openList)]
+                       [i (second top)]
+                       [j (third top)])
+                  (begin
+                    (set! openList (cdr openList))
+                    (vec-set! closedList i j #t)
+                    (successor i j 7)))]))
+
+;Cell-->Popped Cell (i, j) 
+;        N -->  North       (i-1, j) 7
+;        S -->  South       (i+1, j) 6
+;        E -->  East        (i, j+1) 5
+;        W -->  West           (i, j-1) 4
+;        N.E--> North-East  (i-1, j+1) 3
+;        N.W--> North-West  (i-1, j-1) 2
+;        S.E--> South-East  (i+1, j+1) 1
+;        S.W--> South-West  (i+1, j-1) 0
+  
+  ;iter = successor number
+  (define (successor r c iter)
+    (cond [(= iter 0) (void 1)]
+          [else (let* ([i (cond [(= iter 7) (- r 1)]
+                                [(= iter 6) (+ r 1)]
+                                [(= iter 5) r]
+                                [(= iter 4) r]
+                                [(= iter 3) (- r 1)]
+                                [(= iter 2) (- r 1)]
+                                [(= iter 1) (+ r 1)]
+                                [(= iter 0) (+ r 1)])]
+                       [j (cond [(= iter 7) r]
+                                [(= iter 6) r]
+                                [(= iter 5) (+ r 1)]
+                                [(= iter 4) r]
+                                [(= iter 3) (- r 1)]
+                                [(= iter 2) (- r 1)]
+                                [(= iter 1) (+ r 1)]
+                                [(= iter 0) (+ r 1)])]
+                  
+  
+  (cond [(not (isValid (car src) (cdr src))) "Source is invalid"]
+        [(not (isValid (car dest) (cdr dest))) "Destination is invalid"]
+        [(not (or (isUnblocked grid (car src) (cdr src))
+                  (isUnblocked grid (car dest) (cdr dest)))) "Source or the destination is blocked"]
+        [(isDestination (car src) (cdr src) dest) "We are already at the destination"]
+        [else (let* ([i (car src)]
+                     [j (cdr src)])
+                (begin
+                  (vec-set! cellDetails i j (cell i j 0.0 0.0 0.0))
+                  (set! openList (cons (list 0.0 i j) openList))
+                  (looper)))]))
 
 
 
